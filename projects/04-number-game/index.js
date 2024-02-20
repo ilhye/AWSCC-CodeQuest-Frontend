ef50198b380cdce922e576605e0f0f3c570bf92f
ilhye <144883820+ilhye@users.noreply.github.com>
2024-02-20 14:37:33 +0800

    fix format

--- projects/04-number-game/index.js
+++ projects/04-number-game/index.js
@@ -8,66 +8,66 @@ const randContainer = document.querySelector(".rand-container");
 const score = document.getElementById("score");
 const life = document.getElementById("life");
 let scoreCount = 0,
-  lifeCount = 3;
+    lifeCount = 3;
 
 function updateDisplay(
-  message,
-  color,
-  bgColor,
-  fbMessage,
-  feedbackColor,
-  borderColor
+    message,
+    color,
+    bgColor,
+    fbMessage,
+    feedbackColor,
+    borderColor
 ) {
-  result.innerHTML = message;
-  result.style.color = color;
-  randContainer.style.backgroundColor = bgColor;
-  feedback.innerHTML = fbMessage;
-  feedback.style.color = feedbackColor;
-  feedback.style.borderColor = borderColor;
+    result.innerHTML = message;
+    result.style.color = color;
+    randContainer.style.backgroundColor = bgColor;
+    feedback.innerHTML = fbMessage;
+    feedback.style.color = feedbackColor;
+    feedback.style.borderColor = borderColor;
 }
 // Guess number
 guessBtn.addEventListener("click", function (play) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const randNum = Math.floor(Math.random() * 100) + 1;
+    const randNum = Math.floor(Math.random() * 100) + 1;
 
-  if (lifeCount === 0) {
-    alert("No Remaining Life Left");
-  } else if (input.value === "") {
-    alert("Please enter positive integer");
-  }
-
-  if (lifeCount > 0 && input.value > -1 && input.value !== "") {
-    toGuess.innerHTML = randNum;
-
-    if (parseInt(input.value) === randNum) {
-      score.innerHTML = ++scoreCount;
-      updateDisplay("CORRECT", "#3a5a40", "#3a5a40", "", "green", "green");
-    } else {
-      life.innerHTML = --lifeCount;
-      const feedback =
-        input.value > randNum
-          ? "Guess is greater than " + randNum
-          : "Guess is less than " + randNum;
-      updateDisplay("WRONG GUESS", "#BF3131", "#BF3131", feedback);
+    if (lifeCount === 0) {
+        alert("No Remaining Life Left");
+    } else if (input.value === "") {
+        alert("Please enter positive integer");
     }
-    if (lifeCount <= 2) {
-      document.querySelector(".life-board").style.color = "#BF3131";
+
+    if (lifeCount > 0 && input.value > -1 && input.value !== "") {
+        toGuess.innerHTML = randNum;
+
+        if (parseInt(input.value) === randNum) {
+            score.innerHTML = ++scoreCount;
+            updateDisplay("CORRECT", "#3a5a40", "#3a5a40", "", "green", "green");
+        } else {
+            life.innerHTML = --lifeCount;
+            const feedback =
+                input.value > randNum
+                    ? "Guess is greater than " + randNum
+                    : "Guess is less than " + randNum;
+            updateDisplay("WRONG GUESS", "#BF3131", "#BF3131", feedback);
+        }
+        if (lifeCount <= 2) {
+            document.querySelector(".life-board").style.color = "#BF3131";
+        }
     }
-  }
 });
 
 // Reset
 resetBtn.addEventListener("click", function (reset) {
-  result.innerHTML = "";
-  toGuess.innerHTML = "?";
-  input.value = "";
-  feedback.innerHTML = "";
-  randContainer.style.backgroundColor = "#F72798";
-  input.style.borderColor = "";
-  input.style.boxShadow = "";
-  life.innerHTML = "3";
-  score.innerHTML = "0";
-  lifeCount = 3;
-  document.querySelector(".life-board").style.color = "";
+    result.innerHTML = "";
+    toGuess.innerHTML = "?";
+    input.value = "";
+    feedback.innerHTML = "";
+    randContainer.style.backgroundColor = "#F72798";
+    input.style.borderColor = "";
+    input.style.boxShadow = "";
+    life.innerHTML = "3";
+    score.innerHTML = "0";
+    lifeCount = 3;
+    document.querySelector(".life-board").style.color = "";
 });
